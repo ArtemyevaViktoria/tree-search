070bf4dc6f3ac2f12383af82ac4da6a7e4cfcccb
v.artemyeva <v.i.artemyeva@yandex.ru>
2024-12-15 15:30:16 +0300

    Интегрированы форматировщики кода (ESLint + Prettier)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core"
+import { RouterOutlet } from "@angular/router"
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
+  standalone: true,
 })
 export class AppComponent {
-  title = 'tree-search';
+  title = "tree-search"
 }
