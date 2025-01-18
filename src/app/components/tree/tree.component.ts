42f2991dd8c5343b9f1171b4ef611d2609febe2d
v.artemyeva <v.i.artemyeva@yandex.ru>
2025-01-18 10:52:25 +0300

    Добавлено визуальное отображение выбранной позиции

--- src/app/components/tree/tree.component.ts
+++ src/app/components/tree/tree.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, inject, Input, Signal } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Department } from "src/app/models/department.model";
+import { IEmployee } from "src/app/interfaces/employee.interfaces";
+import { DepartmentStoreService } from "src/app/services/store/department-store.service";
 
 @Component({
   selector: "app-tree",
@@ -11,11 +13,22 @@ import { Department } from "src/app/models/department.model";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TreeComponent {
+  /** Сервис хранилища для управления состоянием отедлов */
+  private _departmentStoreService: DepartmentStoreService = inject(DepartmentStoreService);
+
   /** Данные отдела */
   @Input() department: Department = {} as Department;
+
+  /** Выбранная позиция */
+  public selectedPosition: Signal<IEmployee | null> = this._departmentStoreService.selectedPosition;
 
   /** Изменяет состояние развернутости указанного отдела */
   public toggleExpand(department: Department): void {
     department.isExpanded = !department.isExpanded;
+  }
+
+  /** Обработчик клика по строке позиции */
+  public onPositionClick(position: IEmployee) {
+    this._departmentStoreService.setSelectedPosition(position);
   }
 }
--- src/app/services/store/department-store.service.ts
+++ src/app/services/store/department-store.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, inject } from "@angular/core";
+import { Injectable, inject, WritableSignal, signal, Signal } from "@angular/core";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { DepartmentDataService } from "src/app/services/data/department-data.service";
 import { Department } from "src/app/models/department.model";
 import { IDepartmentDTO } from "src/app/interfaces/department.interfaces";
+import { IEmployee } from "src/app/interfaces/employee.interfaces";
 
 /**
  * Сервис хранилища для управления состоянием отделов.
@@ -15,6 +16,12 @@ export class DepartmentStoreService {
   /** Сервис для получения данных отедлов с бэкенда */
   private _departmentDataService: DepartmentDataService = inject(DepartmentDataService);
 
+  /** Выбранная позиция */
+  private _selectedPosition: WritableSignal<IEmployee | null> = signal(null);
+
+  /** Выбранная позиция */
+  public readonly selectedPosition: Signal<IEmployee | null> = this._selectedPosition.asReadonly();
+
   /**
    * Получает данные отедлов с бэкенда и преобразует их в объекты Department.
    * @returns Observable массив объектов Department.
@@ -25,5 +32,10 @@ export class DepartmentStoreService {
       .pipe(
         map((departmentDTO: IDepartmentDTO[]) => departmentDTO.map((dto) => new Department(dto))),
       );
+  }
+
+  /** Установка выбранной позиции */
+  public setSelectedPosition(position: IEmployee) {
+    this._selectedPosition.set(position);
   }
 }
